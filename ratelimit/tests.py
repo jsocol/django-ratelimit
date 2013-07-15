8919f0b4284e6220ea94bfe5633879f9874b8698
Matthias Wahl <matthiaswahl@m7w3.de>
2013-07-15 11:06:53 +0200

    created RateLimitMixin (incl. Tests)

--- ratelimit/tests.py
+++ ratelimit/tests.py
@@ -1,9 +1,14 @@
+import unittest
+
+import django
 from django.core.cache import cache, InvalidCacheBackendError
 from django.test import RequestFactory, TestCase
 from django.test.utils import override_settings
+from django.views.generic import View
 
 from ratelimit.decorators import ratelimit
 from ratelimit.exceptions import Ratelimited
+from ratelimit.mixins import RateLimitMixin
 
 
 class RatelimitTests(TestCase):
@@ -169,3 +174,241 @@ class RatelimitTests(TestCase):
         req = RequestFactory().post('/')
         assert not view(req), 'First unauthenticated request is allowed.'
         assert view(req), 'Second unauthenticated request is limited.'
+
+
+class RateLimitCBVTests(TestCase):
+
+    SKIP_REASON = u"Class Based View supported by Django >=1.4"
+
+    def setUp(self):
+        cache.clear()
+
+    @unittest.skipIf(django.VERSION < (1, 4), SKIP_REASON)
+    def test_limit_ip(self):
+
+        class RLView(RateLimitMixin, View):
+            ratelimit_ip = True
+            ratelimit_method = None
+            ratelimit_rate = '1/m'
+            ratelimit_block = True
+
+        rlview = RLView.as_view()
+
+        req = RequestFactory().get('/')
+        assert rlview(req), 'First request works.'
+        with self.assertRaises(Ratelimited):
+            rlview(req)
+
+    @unittest.skipIf(django.VERSION < (1, 4), SKIP_REASON)
+    def test_block(self):
+
+        class BlockedView(RateLimitMixin, View):
+            ratelimit_ip = True
+            ratelimit_method = None
+            ratelimit_rate = '1/m'
+            ratelimit_block = True
+
+            def get(self, request, *args, **kwargs):
+                return request.limited
+
+        class UnBlockedView(RateLimitMixin, View):
+            ratelimit_ip = True
+            ratelimit_method = None
+            ratelimit_rate = '1/m'
+            ratelimit_block = False
+
+            def get(self, request, *args, **kwargs):
+                return request.limited
+
+        blocked = BlockedView.as_view()
+        unblocked = UnBlockedView.as_view()
+
+        req = RequestFactory().get('/')
+
+        assert not blocked(req), 'First request works.'
+        with self.assertRaises(Ratelimited):
+            blocked(req)
+
+        assert unblocked(req), 'Request is limited but not blocked.'
+
+    @unittest.skipIf(django.VERSION < (1, 4), SKIP_REASON)
+    def test_method(self):
+        rf = RequestFactory()
+        post = rf.post('/')
+        get = rf.get('/')
+
+        class LimitPostView(RateLimitMixin, View):
+            ratelimit_ip = True
+            ratelimit_method = ['POST']
+            ratelimit_rate = '1/m'
+
+            def post(self, request, *args, **kwargs):
+                return request.limited
+            get = post
+
+        class LimitGetView(RateLimitMixin, View):
+            ratelimit_ip = True
+            ratelimit_method = ['POST', 'GET']
+            ratelimit_rate = '1/m'
+
+            def post(self, request, *args, **kwargs):
+                return request.limited
+            get = post
+
+        limit_post = LimitPostView.as_view()
+        limit_get = LimitGetView.as_view()
+
+        assert not limit_post(post), 'Do not limit first POST.'
+        assert limit_post(post), 'Limit second POST.'
+        assert not limit_post(get), 'Do not limit GET.'
+
+        assert limit_get(post), 'Limit first POST.'
+        assert limit_get(get), 'Limit first GET.'
+
+    @unittest.skipIf(django.VERSION < (1, 4), SKIP_REASON)
+    def test_field(self):
+        james = RequestFactory().post('/', {'username': 'james'})
+        john = RequestFactory().post('/', {'username': 'john'})
+
+        class UsernameView(RateLimitMixin, View):
+            ratelimit_ip = False
+            ratelimit_field = 'username'
+            ratelimit_rate = '1/m'
+
+            def post(self, request, *args, **kwargs):
+                return request.limited
+            get = post
+
+        username = UsernameView.as_view()
+        assert not username(james), "james' first request is fine."
+        assert username(james), "james' second request is limited."
+        assert not username(john), "john's first request is fine."
+
+    @unittest.skipIf(django.VERSION < (1, 4), SKIP_REASON)
+    def test_field_unicode(self):
+        post = RequestFactory().post('/', {'username': u'fran\xe7ois'})
+
+        class UsernameView(RateLimitMixin, View):
+            ratelimit_ip = False
+            ratelimit_field = 'username'
+            ratelimit_rate = '1/m'
+
+            def post(self, request, *args, **kwargs):
+                return request.limited
+            get = post
+
+        view = UsernameView.as_view()
+
+        assert not view(post), 'First request is not limited.'
+        assert view(post), 'Second request is limited.'
+
+    @unittest.skipIf(django.VERSION < (1, 4), SKIP_REASON)
+    def test_field_empty(self):
+        post = RequestFactory().post('/', {})
+
+        class EmptyFieldView(RateLimitMixin, View):
+            ratelimit_ip = False
+            ratelimit_field = 'username'
+            ratelimit_rate = '1/m'
+
+            def post(self, request, *args, **kwargs):
+                return request.limited
+            get = post
+
+        view = EmptyFieldView.as_view()
+
+        assert not view(post), 'First request is not limited.'
+        assert view(post), 'Second request is limited.'
+
+    @unittest.skipIf(django.VERSION < (1, 4), SKIP_REASON)
+    def test_rate(self):
+        req = RequestFactory().post('/')
+
+        class TwiceView(RateLimitMixin, View):
+            ratelimit_ip = True
+            ratelimit_rate = '2/m'
+
+            def post(self, request, *args, **kwargs):
+                return request.limited
+            get = post
+
+        twice = TwiceView.as_view()
+
+        assert not twice(req), 'First request is not limited.'
+        assert not twice(req), 'Second request is not limited.'
+        assert twice(req), 'Third request is limited.'
+
+    @unittest.skipIf(django.VERSION < (1, 4), SKIP_REASON)
+    def test_skip_if(self):
+        req = RequestFactory().post('/')
+
+        class SkipIfView(RateLimitMixin, View):
+            ratelimit_rate = '1/m'
+            ratelimit_skip_if = lambda r: getattr(r, 'skip', False)
+
+            def post(self, request, *args, **kwargs):
+                return request.limited
+            get = post
+        view = SkipIfView.as_view()
+
+        assert not view(req), 'First request is not limited.'
+        assert view(req), 'Second request is limited.'
+        del req.limited
+        req.skip = True
+        assert not view(req), 'Skipped request is not limited.'
+
+    @override_settings(RATELIMIT_USE_CACHE='fake-cache')
+    @unittest.skipIf(django.VERSION < (1, 4), SKIP_REASON)
+    def test_bad_cache(self):
+        """The RATELIMIT_USE_CACHE setting works if the cache exists."""
+
+        class BadCacheView(RateLimitMixin, View):
+
+            def post(self, request, *args, **kwargs):
+                return request
+            get = post
+        view = BadCacheView.as_view()
+
+        req = RequestFactory().post('/')
+
+        with self.assertRaises(InvalidCacheBackendError):
+            view(req)
+
+    @unittest.skipIf(django.VERSION < (1, 4), SKIP_REASON)
+    def test_keys(self):
+        """Allow custom functions to set cache keys."""
+        class User(object):
+            def __init__(self, authenticated=False):
+                self.pk = 1
+                self.authenticated = authenticated
+
+            def is_authenticated(self):
+                return self.authenticated
+
+        def user_or_ip(req):
+            if req.user.is_authenticated():
+                return 'uip:%d' % req.user.pk
+            return 'uip:%s' % req.META['REMOTE_ADDR']
+
+        class KeysView(RateLimitMixin, View):
+            ratelimit_ip = False
+            ratelimit_block = False
+            ratelimit_rate = '1/m'
+            ratelimit_keys = user_or_ip
+
+            def post(self, request, *args, **kwargs):
+                return request.limited
+            get = post
+        view = KeysView.as_view()
+
+        req = RequestFactory().post('/')
+        req.user = User(authenticated=False)
+
+        assert not view(req), 'First unauthenticated request is allowed.'
+        assert view(req), 'Second unauthenticated request is limited.'
+
+        del req.limited
+        req.user = User(authenticated=True)
+
+        assert not view(req), 'First authenticated request is allowed.'
+        assert view(req), 'Second authenticated is limited.'
