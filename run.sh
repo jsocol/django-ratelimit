17a2636af0e9b3e0ca030f731468ab26640df355
James Socol <me@jamessocol.com>
2014-09-09 12:49:32 -0400

    Run flake8 with tests.

--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@ export DJANGO_SETTINGS_MODULE="test_settings"
 
 usage() {
     echo "USAGE: $0 [command]"
-    echo "  test - run the jsonview tests"
+    echo "  test - run the ratelimit tests"
+    echo "  flake8 - run flake8"
     echo "  shell - open the Django shell"
     exit 1
 }
@@ -14,6 +15,9 @@ case "$1" in
     "test" )
         shift;
         django-admin.py test ratelimit $@;;
+    "flake8" )
+        shift;
+        flake8 $@ ratelimit/;;
     "shell" )
         django-admin.py shell ;;
     * )
