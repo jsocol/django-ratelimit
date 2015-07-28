99d8b300a175ed725a20a0588c09338a3bb9268e
Carl Meyer <carl@oddbird.net>
2015-07-28 13:57:50 -0600

    Silence flake8 warnings in compat.py

--- ratelimit/compat.py
+++ ratelimit/compat.py
@@ -4,10 +4,10 @@ try:
     def get_cache(name):
         return caches[name]
 except ImportError:  # Django <1.7
-    from django.core.cache import get_cache
+    from django.core.cache import get_cache  # noqa
 
 
 try:
     from importlib import import_module
 except ImportError:  # Python 2.6
-    from django.utils.importlib import import_module
+    from django.utils.importlib import import_module  # noqa
