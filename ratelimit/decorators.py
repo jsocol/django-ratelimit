38625023a303ed9254a1b6476ece8c1c15893deb
Steven Simoni <steven.m.simoni@gmail.com>
2017-07-29 20:36:27 -0700

    Update to work with requests from Django Rest Framework.

--- ratelimit/decorators.py
+++ ratelimit/decorators.py
@@ -4,6 +4,8 @@ from functools import wraps
 
 from django.http import HttpRequest
 
+from rest_framework.request import Request
+
 from ratelimit import ALL, UNSAFE
 from ratelimit.exceptions import Ratelimited
 from ratelimit.utils import is_ratelimited
@@ -17,9 +19,10 @@ def ratelimit(group=None, key=None, rate=None, method=ALL, block=False):
         @wraps(fn)
         def _wrapped(*args, **kw):
             # Work as a CBV method decorator.
-            if isinstance(args[0], HttpRequest):
+            if isinstance(args[0], HttpRequest) or isinstance(args[0], Request):
                 request = args[0]
             else:
+                print args
                 request = args[1]
             request.limited = getattr(request, 'limited', False)
             ratelimited = is_ratelimited(request=request, group=group, fn=fn,
