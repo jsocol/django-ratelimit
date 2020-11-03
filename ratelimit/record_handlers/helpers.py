6e19b9f885fd09fa476df2321deda62d94c11e67
mohammadrabetian <tommyrabetian@gmail.com>
2020-11-03 22:58:56 +0330

    Cache handler for recording exceeded limits in cache

--- ratelimit/record_handlers/helpers.py
+++ ratelimit/record_handlers/helpers.py
@@ -1,6 +1,12 @@
-from typing import Callable
+from datetime import timedelta
+from hashlib import md5
+from typing import Callable, Optional
 
 import ipware.ip
+from django.core.cache import BaseCache, caches
+from django.http import HttpRequest
+from django.utils.module_loading import import_string
+
 from ratelimit.conf import settings
 
 
@@ -45,3 +51,79 @@ def toggleable(func) -> Callable:
             return func(*args, **kwargs)
 
     return inner
+
+
+def get_cache() -> BaseCache:
+    """
+    Get the cache instance RateLimit is configured to use with ``settings.RATELIMIT_USE_CACHE``
+    and use ``'default'`` if not set.
+    """
+
+    return caches[getattr(settings, "RATELIMIT_USE_CACHE", "default")]
+
+
+def get_cache_timeout() -> Optional[int]:
+    """
+    Return the cache timeout interpreted from settings.RATELIMIT_CACHE_RECORD_TIME.
+
+    The cache timeout can be either None if not configured or integer of seconds if configured.
+
+    Notice that the settings.RATELIMIT_CACHE_RECORDS_TIME can be None, timedelta, integer, callable, or str path,
+    and this function offers a unified _integer or None_ representation of that configuration
+    for use with the Django cache backends.
+    """
+
+    cache_timeout = resolve_cache_timeout()
+    if cache_timeout is None:
+        return None
+    return int(cache_timeout.total_seconds())
+
+
+def resolve_cache_timeout() -> Optional[timedelta]:
+    """
+    For use within get_cache_timeout function.
+
+    The return value is either None or timedelta.
+
+    :exception TypeError: if settings.RATELIMIT_CACHE_RECORD_TIME is of wrong type.
+    """
+
+    cache_timeout = settings.RATELIMIT_CACHE_RECORD_TIME
+
+    if isinstance(cache_timeout, int):
+        return timedelta(hours=cache_timeout)
+    if isinstance(cache_timeout, str):
+        return import_string(cache_timeout)()
+    if callable(cache_timeout):
+        return cache_timeout()
+
+    return cache_timeout
+
+
+def make_cache_key(key_components):
+
+    cache_key_components = "".join(value for value in key_components.values() if value)
+    cache_key_digest = md5(cache_key_components.encode()).hexdigest()
+    cache_key = f"drl-{cache_key_digest}"
+
+    return cache_key
+
+
+def get_client_cache_key(request: HttpRequest) -> str:
+    """
+    Build cache key name from request.
+
+    :return cache_key: Hash key that is usable for Django cache backends
+    """
+
+    username = get_client_username(request)
+    ip_address = get_client_ip_address(request)
+    user_agent = get_client_user_agent(request)
+
+    key_components = {
+        "username": username,
+        "ip_address": ip_address,
+        "user_agent": user_agent,
+    }
+
+    return make_cache_key(key_components)
