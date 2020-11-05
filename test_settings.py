11a0b5827429713f992c2118cdc01ff45191cf84
mohammadrabetian <tommyrabetian@gmail.com>
2020-11-05 21:43:48 +0330

    Test database record handler

--- test_settings.py
+++ test_settings.py
@@ -66,3 +66,5 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
 ]
+
+ROOT_URLCONF = "ratelimit.tests.urls"
