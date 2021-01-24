d86b6ee14eb1abc4f5362296d563f22aec0919d0
Szabolcs Barnabás <szabolcs.barnabas@gmail.com>
2021-01-24 23:38:31 +0100

    Fixes django-ratelimit for djangos running on gunicorn behind nginx via proxy_pass.

--- ratelimit/middleware.py
+++ ratelimit/middleware.py
@@ -9,6 +9,15 @@ class RatelimitMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        """
+        Sets REMOTE_ADDR correctly to the client's ip, if REMOTE_ADDR is missing.
+        It happens when django is run with a domain socket server (eg. gunicorn).
+        ref: https://stackoverflow.com/a/34254843/1031191
+        """
+        if not request.META("REMOTE_ADDR", "") and "HTTP_X_FORWARDED_FOR" in request.META:
+            parts = request.META["HTTP_X_FORWARDED_FOR"].split(",", 1)
+            request.META["REMOTE_ADDR"] = parts[0]
+
         return self.get_response(request)
 
     def process_exception(self, request, exception):
