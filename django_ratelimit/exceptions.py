5db7fe487a98a04364143833a7e529a6330ba2be
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2021-08-24 16:31:06 -0400

    Fix import error logic

--- django_ratelimit/exceptions.py
+++ django_ratelimit/exceptions.py
@@ -3,9 +3,9 @@ from django.core.exceptions import PermissionDenied
 
 try:
     from rest_framework.exceptions import Throttled
-
+except ImportError:
     class Ratelimited(PermissionDenied):
         pass
-except ImportError:
+else:
     class Ratelimited(PermissionDenied, Throttled):
         pass
